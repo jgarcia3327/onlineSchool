3ff163cef263e0be8425cf0cf469983082e8793c
J Garcia <jgarcia3327@gmail.com>
2017-09-27 00:49:25 +0800

    Messages teacher translation to english

--- resources/views/messages/index.blade.php
+++ resources/views/messages/index.blade.php
@@ -6,7 +6,13 @@
         <div class="col-md-8 col-md-offset-2">
 
           <div class="panel panel-default">
-              <div class="panel-heading">Tạo tin nhắn</div>
+              <div class="panel-heading">
+                @if (Auth::user()->is_student == 1)
+                  Tạo tin nhắn
+                @else
+                  CREATE MESSAGE
+                @endif
+              </div>
               <div class="panel-body">
                 <form action="{{ url('/messages') }}" method="POST">
                   {{ csrf_field() }}
@@ -26,19 +32,36 @@
                         @endforeach
                       </select>
                     </div>
-                    <textarea class="form-control" name="message" placeholder="Viết tin nhắn"></textarea>
-                    <input class="form-control btn btn-primary" type="submit" value="Gửi" />
+                    @if (Auth::user()->is_student == 1)
+                      <textarea class="form-control" name="message" placeholder="Viết tin nhắn"></textarea>
+                      <input class="form-control btn btn-primary" type="submit" value="Gửi" />
+                    @else
+                      <textarea class="form-control" name="message" placeholder="Write your message"></textarea>
+                      <input class="form-control btn btn-primary" type="submit" value="Send" />
+                    @endif
                   </div>
                 </form>
               </div>
           </div>
 
             <div class="panel panel-default">
-                <div class="panel-heading">Hộp thư</div>
+                <div class="panel-heading">
+                  @if (Auth::user()->is_student == 1)
+                    Hộp thư
+                  @else
+                    MY MESSAGES
+                  @endif
+                </div>
                 <div class="panel-body">
                   <ul class="list-group">
                     @if($messages == null || count($messages) <= 0)
-                      <li class="list-group-item"><span class="badge">0</span> Không có tin nhắn nào </li>
+                      <li class="list-group-item"><span class="badge">0</span>
+                        @if (Auth::user()->is_student == 1)
+                          Không có tin nhắn nào
+                        @else
+                          No messages found.
+                        @endif
+                      </li>
                     @else
                       @foreach($messages AS $v)
                         <li class="list-group-item">
